ad14031c4a7c24fc0b646fcce2464ce452a1a2a8
henriquelsx <henriquelsx@hotmail.com>
2022-06-10 15:30:29 -0300

    Can add new tasks now

--- Todo List - Initial/scripts.js
+++ Todo List - Initial/scripts.js
@@ -12,7 +12,22 @@ var todos = [
 const todosApp = {
     data(){
         return{
-            todos:window.todos
+            todos:window.todos,
+            newTodo: {
+                done: false
+            }
+        }
+    },
+    methods: {
+        addTodo: function(){
+            if(this.newTodo.text){
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+                };
+            } else {
+                alert('Insira algo na área em branco')
+            }
         }
     }    
 };
